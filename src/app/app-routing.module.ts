9d7605f81b75e122a610339694cf76896363be8e
sam <aspirantbelosheev@tut.by>
2023-08-14 12:01:09 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {AdventureComponent} from "./adventure/adventure.component";
+import {StrategyComponent} from "./strategy/strategy.component";
+import {MindComponent} from "./mind/mind.component";
+import {SurvivelComponent} from "./survivel/survivel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'adventure', component: AdventureComponent},
+  {path: 'strategy', component: StrategyComponent},
+  {path: 'mind', component: MindComponent},
+  {path: 'survivel', component: SurvivelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { StrategyComponent } from './strategy/strategy.component';
+import { MindComponent } from './mind/mind.component';
+import { AdventureComponent } from './adventure/adventure.component';
+import { SurvivelComponent } from './survivel/survivel.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    StrategyComponent,
+    MindComponent,
+    AdventureComponent,
+    SurvivelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
