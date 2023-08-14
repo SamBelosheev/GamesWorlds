f0c4a7b7c02a3ea7e5f73401d8140ef2ddcda46c
sam <aspirantbelosheev@tut.by>
2023-08-14 17:06:09 +0300

    second commit

---
+++ src/app/survivel/survivel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survivel',
+  templateUrl: './survivel.component.html',
+  styleUrls: ['./survivel.component.scss']
+})
+export class SurvivelComponent {
+
+}
